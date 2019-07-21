48494c52212d487db8d4df8755da977037343fb9
Peerapat A <nuboat@gmail.com>
2019-07-21 12:02:44 +0700

    Refactor

---
+++ src/main/scala/yoda/utilities/Closer.scala
@@ -0,0 +1,19 @@
+package yoda.utilities
+
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.language.reflectiveCalls
+import scala.util.Try
+
+/**
+  * Created by Peerapat A on Mar 18, 2017
+  */
+trait Closer extends LazyLogging {
+
+  def closer[T <: {def close(): Unit}, R](resource: T)(block: T => R): R = try {
+    block(resource)
+  } finally {
+    Try(resource.close()).failed foreach { t => logger.warn(t.getMessage)}
+  }
+
+}
